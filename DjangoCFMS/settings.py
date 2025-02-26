a065d145364dc1f2bfaf777f137d1f8480240cf6
Christian Kiprotich <kiprotichchristian@gmail.com>
2025-02-26 11:04:18 +0300

    Added the serializer file with all the models serialized

--- DjangoCFMS/settings.py
+++ DjangoCFMS/settings.py
@@ -38,11 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,3 +126,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+CORS_ALLOW_ALL_ORIGINS = True
---
+++ main/serializers.py
@@ -0,0 +1,80 @@
+from rest_framework import serializers
+from main.models import (
+    Employee,
+    Salary,
+    Credit,
+    Repayment,
+    Wage,
+    Input,
+    BatchPrice,
+    Farm,
+    Owner,
+    Services,
+    Output,
+    Tool,
+    Cost)
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
+class SalarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Salary
+        fields = '__all__'
+
+class CreditSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Credit
+        fields = '__all__'
+
+class RepaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Repayment
+        fields = '__all__'
+
+class WageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wage
+        fields = '__all__'
+
+class Inputerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Input
+        fields = '__all__'
+
+class BatchPriceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BatchPrice
+        fields = '__all__'
+
+class FarmSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Farm
+        fields = '__all__'
+
+class OwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Owner
+        fields = '__all__'
+
+class ServicesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Services
+        fields = '__all__'
+
+class OutputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Output
+        fields = '__all__'
+
+class ToolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tool
+        fields = '__all__'
+
+class CostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cost
+        fields = '__all__'
