c46c54cd28fcad89ee3b05b2a1f31de08d53ef00
Christian Kiprotich <kiprotichchristian@gmail.com>
2025-03-10 13:21:20 +0300

    Changed the database setting to fit python anywhere production database

--- DjangoCFMS/settings.py
+++ DjangoCFMS/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'DjangoCFMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'krisRono$CFMS',
+        'USER': 'krisRono',
+        'PASSWORD': 'RcHzCAG7x>%W*?T',
+        'HOST': 'krisRono.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 
